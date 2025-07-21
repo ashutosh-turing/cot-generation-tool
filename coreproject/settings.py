18e1458ba25bc7ab0645ae9e0374001b1d71d042
Ashutosh <ashutosh.singh@turing.com>
2025-07-21 17:23:50 +0530

    fix(*): settings

--- coreproject/settings.py
+++ coreproject/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'eval', 'static'),
+    # os.path.join(BASE_DIR, 'eval', 'static'),
     os.path.join(BASE_DIR, 'processor', 'static'),
     os.path.join(BASE_DIR, 'static'),
 ]
