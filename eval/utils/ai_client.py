13dad97e1c20e57a2f9f75aef0bdff5d38b772c0
Ashutosh <ashutosh.singh@turing.com>
2025-07-25 14:58:41 +0530

    fix(*): 40-mini support

--- eval/utils/ai_client.py
+++ eval/utils/ai_client.py
@@ -126,7 +126,10 @@ class OpenAIClient(BaseAIClient):
         
         # Use max_tokens parameter if provided, else fall back to model_instance or default
         if max_tokens is not None:
-            params["max_tokens"] = max_tokens
+            if 'o4-mini'in self.model_name:
+                params["max_completion_tokens"] = max_tokens
+            else:
+                params["max_tokens"] = max_tokens
         elif self.model_instance and self.model_instance.max_tokens:
             params["max_tokens"] = self.model_instance.max_tokens
         else:
